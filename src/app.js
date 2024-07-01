8a12a6d979a45811b92942e063b2c1a075e71044
NEXlvl01 <armanthakur2004@gmail.com>
2024-07-01 14:12:37 +0530

    started creating routes

--- src/app.js
+++ src/app.js
@@ -11,4 +11,11 @@ app.use(express.urlencoded()); //For Accepting Data from URL
 app.use(express.static("public")); //For storing static data in public folder
 app.use(cookieParser());
 
+//import Routes
+
+import userRouter from "./routes/user.routes.js";
+
+//routes Declaration
+app.use("/api/v1/users",userRouter);
+
 export default app;
